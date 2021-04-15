aac60907ad7e3a2aba57270f6996b5e1a4bf8d37
Laura <lkzailac@gmail.com>
2021-04-15 13:42:33 -0400

    finished phase 1

--- src/components/Greenhouse/Greenhouse.js
+++ src/components/Greenhouse/Greenhouse.js
@@ -1,15 +1,20 @@
 import dayImage from './images/greenhouse-day.jpg';
 import nightImage from './images/greenhouse-night.jpg';
+import { useTheme } from '../../context/ThemeContext';
 
 import './Greenhouse.css';
 
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
 
+
 function Greenhouse() {
+  const { themeName, setThemeName } = useTheme();
+  const dayOrNight = themeName === 'day' ? dayImage : nightImage
+
   return (
     <section>
-      <img className='greenhouse-img' src={dayImage} alt='greenhouse' />
+      <img className='greenhouse-img' src={dayOrNight} alt='greenhouse' />
       <LightSwitch />
       <ClimateStats />
     </section>
